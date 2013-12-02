eca56c52de0f4a4925c1a7ace873c432f183ab50
lyuba <kolosovska@gmail.com>
2013-12-02 06:18:47 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EcoshopLanding::Application.config.secret_key_base = '72d357facf0eb51e8dc56bf5bb8ee8e558d9ad78f67694a0b33ee4d3a17f60a7d58511354b2dacfc48c621dc108c70fce888dab779e877b24aa30a7edb6b0418'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EcoshopLanding::Application.config.session_store :cookie_store, key: '_ecoshop-landing_session'
